6e958894c56da2f03ec01317e971106aedc7c5e7
Oliver Niemi <oliver.niemi@aalto.fi>
2024-12-16 13:50:42 +0200

    Unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,19 +12,19 @@ const StatsUnit = (props) => {
   return <p>{props.name} {props.state} {props.unit}</p>
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
   const total = good + neutral + bad
-
+  if (total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button name={'good'} state={good} setState={setGood}></Button>
-      <Button name={'neutral'} state={neutral} setState={setNeutral}></Button>
-      <Button name={'bad'} state={bad} setState={setBad}></Button>
       <h1>statistics</h1>
       <Stats name={'good'} state={good}></Stats>
       <Stats name={'neutral'} state={neutral}></Stats>
@@ -32,6 +32,23 @@ const App = () => {
       <Stats name={'all'} state={total}></Stats>
       <Stats name={'average'} state={(good - bad) / (total)}></Stats>
       <StatsUnit name={'positive'} state={(good / total) * 100} unit={'%'}></StatsUnit>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button name={'good'} state={good} setState={setGood}></Button>
+      <Button name={'neutral'} state={neutral} setState={setNeutral}></Button>
+      <Button name={'bad'} state={bad} setState={setBad}></Button>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
