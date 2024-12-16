4678844b23e8745772ff501ad62bb67cc109cb8b
Oliver Niemi <oliver.niemi@aalto.fi>
2024-12-16 14:07:26 +0200

    Unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,38 +1,34 @@
 import { useState } from 'react'
 
-const Button = (props) => {
-  return <button onClick={() => props.setState(props.state + 1)}>{props.name}</button>
+const Button = (props) => <button onClick={() => props.setState(props.state + 1)}>{props.name}</button>
+
+const StatisticLine = (props) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{props.text}</td> 
+        <td>{props.value}</td>
+      </tr>
+    </tbody>
+  )
 }
 
-const Stats = (props) => {
-  return <p>{props.name} {props.state}</p>
-}
-
-const StatsUnit = (props) => {
-  return <p>{props.name} {props.state} {props.unit}</p>
-}
-
-const Statistics = (props) => {
-  const { good, neutral, bad } = props
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   if (total === 0) {
     return (
-      <div>
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </div>
+      <div>No feedback given</div>
     )
   }
   return (
-    <div>
-      <h1>statistics</h1>
-      <Stats name={'good'} state={good}></Stats>
-      <Stats name={'neutral'} state={neutral}></Stats>
-      <Stats name={'bad'} state={bad}></Stats>
-      <Stats name={'all'} state={total}></Stats>
-      <Stats name={'average'} state={(good - bad) / (total)}></Stats>
-      <StatsUnit name={'positive'} state={(good / total) * 100} unit={'%'}></StatsUnit>
-    </div>
+    <table>
+      <StatisticLine text="good" value={good}></StatisticLine>
+      <StatisticLine text="neutral" value={neutral}></StatisticLine>
+      <StatisticLine text="bad" value={bad}></StatisticLine>
+      <StatisticLine text="all" value={total}></StatisticLine>
+      <StatisticLine text="average" value={(good - bad) / (total)}></StatisticLine>
+      <StatisticLine text="positive" value={String((good / total) * 100) + "%"}></StatisticLine>
+    </table>
   )
 }
 
@@ -48,6 +44,7 @@ const App = () => {
       <Button name={'good'} state={good} setState={setGood}></Button>
       <Button name={'neutral'} state={neutral} setState={setNeutral}></Button>
       <Button name={'bad'} state={bad} setState={setBad}></Button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
