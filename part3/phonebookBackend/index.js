f82a6bb3b3b0370028c14797faab69a9f874b334
Oliver Niemi <oliver.niemi@aalto.fi>
2025-01-02 19:06:27 +0200

    Phonebook database step 5

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -77,6 +77,22 @@ app.post('/api/persons', (request, response, next) => {
     })
   })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+  })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
