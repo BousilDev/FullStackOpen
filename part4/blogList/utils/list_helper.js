66faac370434c7d97396bdc458caf106be6fb752
Oliver Niemi <oliver.niemi@aalto.fi>
2025-01-06 20:08:13 +0200

    4.4 Helper functions and unit tests

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    const likesReducer = (sum, item) => {
+        return sum + item.likes
+      }
+    return blogs.reduce(likesReducer, 0)
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
