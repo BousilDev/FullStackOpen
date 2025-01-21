b432471f73f89111fc9d7f39f64fcf2f173c218d
Oliver Niemi <oliver.niemi@aalto.fi>
2025-01-21 20:19:15 +0200

    5.4 Blog list frontend step 4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -7,12 +8,33 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState(null)
   const [user, setUser] = useState(null)
+
+  const showMessage = msg => {
+    setMessage(msg)
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -21,11 +43,27 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      
+      setMessageType('error')
+      showMessage('Wrong username or password')
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    try {
+      window.localStorage.removeItem('loggedUser')
+      setUser(null)
+    } catch (exception) {
+      setMessageType('error')
+      showMessage('Logout failed')
     }
   }
 
@@ -53,24 +91,75 @@ const App = () => {
     </form>      
   )
 
-  if (user === null) {
+  const inputField = (value, name, setValue) => {
     return (
       <div>
-        <h2>Log in to application</h2>
-        {loginForm()}
+        {name}:
+          <input
+          type="text"
+          value={value}
+          name={name}
+          onChange={({ target }) => setValue(target.value)}
+        />
       </div>
     )
   }
 
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = {
+        title: title,
+        author: author,
+        url: url
+      }
+      await blogService.create(newBlog)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      blogService.getAll().then(blogs =>
+        setBlogs( blogs )
+      )
+      setMessageType('success')
+      showMessage(`${newBlog.title} by ${newBlog.author} added`)
+    } catch (exception) {
+      setMessageType('error')
+      showMessage('Adding new blog failed')
+    }
+  }
+
+  const blogForm = () => (
+    <form onSubmit={handleNewBlog}>
+      {inputField(title, "title", setTitle)}
+      {inputField(author, "author", setAuthor)}
+      {inputField(url, "url", setUrl)}
+      <button type="submit">create</button>
+    </form>      
+  )
+
   return (
     <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Notification message={message} className={messageType} />
+    
+      {user === null ?
+          <div>
+            <h2>Log in to application</h2>
+            {loginForm()}
+          </div>
+      :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>logout</button>
+          <h2>create new</h2>
+          {blogForm()}
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
     </div>
-  )
+    )
 }
 
 export default App
